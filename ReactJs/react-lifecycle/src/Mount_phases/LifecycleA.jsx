dc4c69430740d239e12dcb60aad87874f2a42ddb
himamphb <himamphb124@gmail.com>
2022-12-16 15:42:14 +0530

    react-lifecycle

---
+++ ReactJs/react-lifecycle/src/Mount_phases/LifecycleA.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+export default class LifecycleA extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
